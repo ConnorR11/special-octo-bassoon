2f2a072d0aeb76568c24f2a68ff8a865b84f8b7b
ConnorR11 <142886022+ConnorR11@users.noreply.github.com>
2026-08-31 22:41:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
     const { data, error: supabaseError } = await supabase
       .from("sold_contracts")
       .select("*")
-      .order("sale_date", { ascending: true })
+      .order("sale_date", { ascending: false })
 
     if (supabaseError) {
       setError(supabaseError.message)
