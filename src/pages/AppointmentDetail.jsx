9a61124fc5050ce018e018cd9236f6561016ebe9
ConnorR11 <142886022+ConnorR11@users.noreply.github.com>
2026-09-04 13:40:19 +0100

    Update AppointmentDetail.jsx

--- src/pages/AppointmentDetail.jsx
+++ src/pages/AppointmentDetail.jsx
@@ -15,7 +15,7 @@ import {
 
 import { supabase } from "../lib/supabase"
 import EPVSCalculator from "../EPVSCalculator"
-import { GenerateSolarContract } from "../contracts-temp/GenerateSolarContract"
+import { GenerateSolarContract } from "../contracts/GenerateSolarContract"
 
 function AppointmentDetail({
   appointment,
