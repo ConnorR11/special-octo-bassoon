293e0e8ce78d741e79e0b14e77bffa3d8fd0ac00
ConnorR11 <142886022+ConnorR11@users.noreply.github.com>
2026-09-22 10:08:09 +0100

    Render sales presenter through a document portal

--- src/components/SalesPresenter.jsx
+++ src/components/SalesPresenter.jsx
@@ -1,1 +1,8 @@
-export { default } from "./DatabaseSalesPresenter"
+import React from "react"
+import { createPortal } from "react-dom"
+import DatabaseSalesPresenter from "./DatabaseSalesPresenter"
+
+export default function SalesPresenter(props) {
+  if (typeof document === "undefined") return null
+  return createPortal(<DatabaseSalesPresenter {...props} />, document.body)
+}
